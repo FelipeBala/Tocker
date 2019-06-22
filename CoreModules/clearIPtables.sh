82a4b553a86ad8f0b8f684cb76198d7a1a659b5d
Felipe Bala <felipebalabanian@gmail.com>
2019-06-22 17:19:51 -0300

    Starting Dynamic Security Analyses modifications

--- CoreModules/clearIPtables.sh
+++ CoreModules/clearIPtables.sh
@@ -1,5 +1,8 @@
 iptables -t filter -F RULES 
 iptables -t filter -F SUBNETS
+iptables -t filter -F INTERNETRULES
 iptables -t filter -D FORWARD -j SUBNETS
 iptables -t filter -X RULES 
+iptables -t filter -X INTERNETRULES
 iptables -t filter -X SUBNETS
+
